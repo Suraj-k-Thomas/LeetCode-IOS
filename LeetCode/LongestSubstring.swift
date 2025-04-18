b69fae7b60d15f2fe08a1aca3488b0a4ef17dce9
surajkthomas <ksurajt@gmail.com>
2025-04-18 19:05:12 +0530

    maxlength of substring code  using set and moving windows

--- LeetCode/LongestSubstring.swift
+++ LeetCode/LongestSubstring.swift
@@ -62,3 +62,55 @@ func lengthOfLongestSubstring(_ s: String) -> Int {
     
     return maxLength
 }
+
+
+/**
+ 
+ solution 2 - using sets and moving windows
+ 
+ create set of characters
+ 
+ 2 pointers - left ,right = 0
+ 
+ while right < s.end{
+ let char = s.right
+ if ! charset . contains char{
+ 
+ charset.add string.right// add char to charset
+ maxlength = max(chharset, distancefromlefttoright,1)
+ right = s.index after(right)// increment right
+ 
+ }else{
+ 
+ charset.removefrom (s[left]) // remove from string.leftIndex
+ left = s.index after left // increment left
+ 
+ }}
+
+ */
+
+
+
+func lengthOfLongestSubstringUsingSet(_ s: String) -> Int {
+    var maxLength = 0
+    var rightIndex = s.startIndex
+    var leftIndex = rightIndex
+    
+    var Charset: Set<Character> = []
+    while rightIndex < s.endIndex {
+        let char = s[rightIndex]
+        
+        if !Charset.contains(char){
+            Charset.insert(char)
+            maxLength = max(maxLength, s.distance(from: leftIndex, to: rightIndex) + 1)
+            rightIndex = s.index(after: rightIndex)
+        }else {
+            
+            Charset.remove(s[leftIndex])
+            leftIndex = s.index(after: leftIndex)
+        }
+    }
+    
+    return maxLength
+    
+}
--- LeetCodeTests/LongestSubstringTests.swift
+++ LeetCodeTests/LongestSubstringTests.swift
@@ -21,11 +21,37 @@ final class LongestSubstringTests: XCTestCase {
         
         let string = "abcabcbb"
         
-        XCTAssertEqual(lengthOfLongestSubstring(string), 3)
+        XCTAssertEqual(lengthOfLongestSubstring(string), 3)    }
+    
+    func testLongestSubstringWithValidStringusingSetandWindow() {
         
+        let string = "abcdabcbb"
+        
+        XCTAssertEqual(lengthOfLongestSubstringUsingSet(string), 4)
     }
     
 }
 
 
+//
+//func lengthOfLongestSubstringUsingSet(_ s: String) -> Int {
+//    var charSet = Set<Character>()
+//    var left = s.startIndex
+//    var maxLength = 0
+//
+//    var right = left
+//    while right < s.endIndex {
+//        let char = s[right]
+//        if !charSet.contains(char) {
+//            charSet.insert(char)
+//            maxLength = max(maxLength, s.distance(from: left, to: right) + 1)
+//            right = s.index(after: right)
+//        } else {
+//            charSet.remove(s[left])
+//            left = s.index(after: left)
+//        }
+//    }
+//
+//    return maxLength
+//}
 
