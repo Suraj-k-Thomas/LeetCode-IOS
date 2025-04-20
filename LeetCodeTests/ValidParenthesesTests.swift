d8e993da0b76920fa4bc15fd417e77ab349b1cda
surajkthomas <ksurajt@gmail.com>
2025-04-20 11:45:59 +0530

    production code moved to valid paranthesis file

--- LeetCodeTests/ValidParenthesesTests.swift
+++ LeetCodeTests/ValidParenthesesTests.swift
@@ -7,6 +7,7 @@
 
 import Testing
 import XCTest
+@testable import LeetCode
 class ValidParenthesesTests:XCTestCase {
     
     
@@ -21,56 +22,6 @@ class ValidParenthesesTests:XCTestCase {
     }
 }
 
-func validBraces(_ s: String) -> Bool {
-
-    let pairs: [Character: Character] = [")":"(", "]":"[" ,"}":"{"]
-    var stack: [Character] = []
-    for char in s {
-        
-        if char == "(" || char == "[" || char == "{" {
-            stack.append(char)
-        }else  if let match = pairs[char]{
-            
-            
-            if   stack.isEmpty || stack.popLast() != match{
-                return false
-                
-            }
-        }
-        print("stack \(stack)")
-
-    }
-    
-    return stack.isEmpty
-    
-}
 
 
-/**
- 
- Given a string s containing just the characters '(', ')', '{', '}', '[', and ']', determine if the input string is valid.
- 
- ✅ A valid string must:
- Open brackets must be closed by the same type of bracket
- Brackets must be closed in the correct order
- Every closing bracket must have a corresponding open bracket
 
- Input: "()"       → Output: true
- Input: "()[]{}"   → Output: true
- Input: "(]"       → Output: false
- Input: "([)]"     → Output: false
- Input: "{[]}"     → Output: true
-
- */
-
-/*
- solution
- * using stack
- * use dictionary to store  closing  and the  matching opening paranthesis.
- *Traverse through string , if char equals opening string add to stack.
- *else pop the top of the stack
- 
- * Check if current item on top of stack is theopening paranthesis  of the current  char .
- based on this condition return true or false
- 
- */
